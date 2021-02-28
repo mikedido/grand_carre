5e4431888e0a3c9ef7ae1a9975cd68547851ad70
Mahdi GUEFFAZ <you@example.com>
2021-02-28 22:51:36 +0100

    unittest file

---
+++ app/tests/test_find_square.py
@@ -0,0 +1,22 @@
+import unittest
+from app import app
+
+
+class TestfindSquare(unittest.TestCase):
+
+    def test_get_solution(self):
+        pass
+
+
+    def test_find_square(self):
+        pass
+
+
+    def test_get_solution_point(self):
+        pass
+
+
+    def test_get_best_solution(self):
+        pass
+
+
