bd4170449dc24f9b7a359d0bd813cdec349c638d
Mahdi GUEFFAZ <you@example.com>
2021-03-01 13:44:42 +0100

    Add functionnal test

--- app/routes/api.py
+++ app/routes/api.py
@@ -2,9 +2,10 @@ from app import app
 import json
 import sys
 from flask import Flask
-from ..services.find_square import find_square
+from ..services.find_square import find_square, get_matrix
 
 @app.route('/<string:filename>')
 def main(filename):
-    return json.dumps(find_square('app/map_gen_files/'+filename))
+    matrix, first_line = get_matrix('app/map_gen_files/'+filename)
+    return json.dumps(find_square(matrix, first_line))
     
