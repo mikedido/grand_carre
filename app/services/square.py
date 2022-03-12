4587d8cc9256ecef0df28992dcefb7356d521505
mikedido <mahdigueffaz@gmail.com>
2022-03-12 14:49:01 +0100

    Add square class

---
+++ app/services/square.py
@@ -0,0 +1,90 @@
+
+class Square:
+    """
+    Square
+    """
+    def __init__(self, matrix, obstacle_caractere, empty_caractere):
+        self.matrix = matrix
+        self.obstacle_caractere = obstacle_caractere
+        self.empty_caractere = empty_caractere
+
+
+    def get_all_square(self):
+        solution = []
+
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[0])) :
+                if self.empty_caractere == self.matrix[i][j] :
+                    solution.append(self.get_square_by_point(i, j))
+        
+        return solution
+
+    
+    def get_square_by_point(self, i, j):
+        """
+        Return a square by point(i, j)
+        """
+        test_number = 0
+        
+        while test_number < min((len(self.matrix) - i), (len(self.matrix[0]) - j)):
+            for line in range(i, i + test_number) :
+                for column in range(j, j + test_number) :
+                    if self.obstacle_caractere == self.matrix[line][column]:
+                        return (i, j, test_number-1)
+            test_number += 1
+
+        return (i, j, test_number)
+
+    def get_big_square(self, matrix_solution):
+        #get the biggest value
+        bigest_carre = (0,0,0)
+
+        for solution in matrix_solution:
+            if solution is not None :
+                if solution[2] == bigest_carre[2] :
+                    if solution[0] < bigest_carre[0]:
+                        bigest_carre = solution
+                    elif solution[1] < bigest_carre[1]:
+                        bigest_carre = solution
+                elif solution[2] > bigest_carre[2] :
+                    bigest_carre = solution
+
+        return bigest_carre
+
+
+
+if __name__ == '__main__' :
+    #init matrix
+    matrice = [
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    ]
+    #show the result
+    print('hello world')
+    square = Square(matrice, '.', 0)
+    solution = square.get_all_square()
+    print(solution)
+    print('best solution ', square.get_big_square(solution))
